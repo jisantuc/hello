d93e20a48877595e07bc09f0cf26302eb652405d
James Santucci <james.santucci@gmail.com>
2017-09-19 08:35:53 -0400

    Functional summer ch13

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import DogsRule
+import Hello
+import System.IO
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  hSetBuffering stdout NoBuffering
+  putStr "Please input your name: "
+  name <- getLine
+  sayHello name
+  dogs
